fbd0a50a94b50b88aa8ddcb98c56cb52f300b082
gabrielvnw <gabrimoraes2004@gmail.com>
2022-01-17 16:25:46 -0300

    introdução a lógica de programação

---
+++ script.js
@@ -0,0 +1,17 @@
+// Crie uma variável que receba um nome;
+let name ="Gabriel"
+// Crie uma variável que receba um número;
+let number = 17
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let year = 2022
+let ano = year -= number
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(number+ano)
+//Mostre no console a mensagem "Estou mexendo no console.log";
+let text = "Estou mexendo no console.log"
+console.log(text)
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+console.log(quartaFeira)
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
